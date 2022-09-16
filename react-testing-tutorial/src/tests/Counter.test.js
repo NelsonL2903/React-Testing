ebfbe8cb76d5268ca3d37560ecf8481e6e7f4563
Nelson Loop <nelsonploop@gmail.com>
2022-09-16 12:33:32 -0400

    Added a test to check the decrement counter button.

--- react-testing-tutorial/src/tests/Counter.test.js
+++ react-testing-tutorial/src/tests/Counter.test.js
@@ -1,18 +1,34 @@
 import { render, fireEvent, screen } from "@testing-library/react";
 import Counter from "../components/Counter";
 
-//test block
-test("increments counter", () => {
-// render the component on virtual dom
-render(<Counter />);
+describe('Application root', () => {
+    it('increments counter', () => {
+        // render the component on virtual dom
+        render(<Counter />);
 
-//select the elements you want to interact with
-const counter = screen.getByTestId("counter");
-const incrementBtn = screen.getByTestId("increment");
+        //select the elements you want to interact with
+        const counter = screen.getByTestId("counter");
+        const incrementBtn = screen.getByTestId("increment");
 
-//interact with those elements
-fireEvent.click(incrementBtn);
+        //interact with those elements
+        fireEvent.click(incrementBtn);
 
-//assert the expected result
-expect(counter).toHaveTextContent("1");
+        //assert the expected result
+        expect(counter).toHaveTextContent("1");
+    });
+
+    it('decrements counter', () => {
+        // render the component on virtual dom
+        render(<Counter />);
+
+        //select the elements you want to interact with
+        const counter = screen.getByTestId("counter");
+        const decrementBtn = screen.getByTestId("decrement");
+
+        //interact with those elements
+        fireEvent.click(decrementBtn);
+
+        //assert the expected result
+        expect(counter).toHaveTextContent("-1");
+    });
 });
